696437849a34cad206228999c97c0cacbe3811c7
Darkdevil-tech-tech <15adhinath@gmail.com>
2025-11-11 19:01:50 +0530

    Set basename for BrowserRouter to '/voiceofcode'

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/voiceofcode">
         <AuthProvider>
           <Routes>
             <Route path="/" element={<Index />} />
